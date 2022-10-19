48ae19ce33d0eaee434959b539df493d8da09e23
Christopher Marzulli <chriszulli15@gmail.com>
2022-10-19 12:21:49 -0400

    commit tree 9267ebbc6c570b7ef9ec41efcfff928da8d32019

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  validate :click_baity
+
+  # bait_words = ["Won't Believe", "Secret", "Guess", "Top [number]"]
+
+  def click_baity
+    unless title.include?("Won't Believe" || "Secret" || "Guess" || "Top [number]")
+      errors.add(:title, "not click-baity enough")
+    end
+  end
+
 end
